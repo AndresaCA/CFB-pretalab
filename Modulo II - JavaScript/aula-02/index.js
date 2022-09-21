54de9299f7819ffc41f866d66f8032b3c19b83b0
AndresaCA <avelarav@gmail.com>
2022-09-20 21:27:46 -0300

    aula 02

---
+++ Modulo II - JavaScript/aula-02/index.js
@@ -0,0 +1,18 @@
+//const alunas = ['Anna', 1, 'Bia', 2, 'Carla', 3]
+
+//console.log(alunas[1])
+
+/*const notas = [1, 2, 3]
+notas.forEach((nomes) => {
+    console.log(nomes)
+})*/
+
+//const langue = ['JS', 1, 2, 3, 'HTML', 'CSS']
+
+/*langue.forEach((sample) => {
+    if (isNaN(sample)) {
+        console.log(sample)
+    }
+})*/
+
+
