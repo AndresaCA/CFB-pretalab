6a06b7a5ec31a17f873925961416d39d7c92fa0e
AndresaCA <avelarav@gmail.com>
2022-10-01 11:24:18 -0300

    aula 05

--- Modulo II - JavaScript/aula-04/lacos.js
+++ Modulo II - JavaScript/aula-04/lacos.js
@@ -10,7 +10,7 @@ while (salario < 5000) {
     console.log('O salário é de R$ ' + salario)
 }*/
 
-const alunas = [
+/*const alunas = [
     {aluno: 'Alcina', nota:10},
     {aluno: 'Bella', nota: 9},
     {aluno:'Cassandra', nota: 8},
@@ -27,5 +27,17 @@ do {
 } while (index < totAlunas)
 let media = somaNotas / totAlunas
 
-console.log(media)
+console.log(media)*/
+
+// Metodo de repetição DoWhilie
+let controle = 0 // primeiramente a variavel é definida
+do { // faça isso
+    console.log('Método while') //imprima
+    controle += 1 // some mais um
+} while (controle <3) // enquanto o valor da variavel for menor que esse valor
+
+// Método de repetição for
+for (let i = 0; i < 5; i+=1) {
+   console.log('Método for')
+}
 
