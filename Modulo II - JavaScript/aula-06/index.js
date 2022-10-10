7a914d084106b28ab12833e93ac5e97f52e01a69
AndresaCA <avelarav@gmail.com>
2022-10-10 13:04:43 -0300

    aula 06

---
+++ Modulo II - JavaScript/aula-06/index.js
@@ -0,0 +1,11 @@
+/*function comum (parametro, parametro2) {
+    //escopo da função
+}
+
+const aeroFun = (parametro, parametro2, parametro3) => {
+    //escopa da função
+}
+
+comum (1, 2)
+aeroFun (1, 2, 3) */
+
