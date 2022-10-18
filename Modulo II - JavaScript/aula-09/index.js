33a872148f8a043c45ecc1f18dd7bf0ce86759ac
AndresaCA <avelarav@gmail.com>
2022-10-18 19:23:47 -0300

    aula 09

--- Modulo II - JavaScript/aula-09/index.js
+++ Modulo II - JavaScript/aula-09/index.js
@@ -1,12 +1,11 @@
 function escreverMensagem () {
     const textoDigitado = document.getElementById('nome').value //cria a variavel textoDigitado e pega o valor digitado no input nome
-    const bemVinda = document.getElementById('bemVinda').innerHTML //cria a variavel bemVinda
-    console.log(bemVinda)
-    if (bemVinda.split(':')[1].length > 1) {
-        console.log(bemVinda.split(':'))
+    const bemVinda = document.getElementById('bemVinda').innerHTML //cria a variavel bemVinda e selecina o innerHTML do paragrafo com a id vemVinda
+    console.log(bemVinda.length)
+    if (bemVinda.split(':')[1].length > 1) { // divide o paragrafo bemVinda a partir dos dois pontos, separando eles em dois elementos de uma array
         document.getElementById('bemVinda').innerHTML = `Bem-vinda: ${textoDigitado}`
     } else {
-        document.getElementById('bemVinda').innerHTML += textoDigitado
+        document.getElementById('bemVinda').innerHTML += textoDigitado // isso garante que o texto no paragrafo la do html nao seja substituido, ao inves disso voces acrescente o texto digitado a ele
     }
 }
 
